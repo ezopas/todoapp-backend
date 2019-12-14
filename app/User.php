aadde8faa52ed6a78905be77142417f4bf958aae
ezopas <ezostrauskas@gmail.com>
2019-12-14 21:45:07 +0200

    Added JWT autentification package

--- app/User.php
+++ app/User.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -35,4 +36,18 @@ class User extends Authenticatable
         return $this->hasMany(Task::class);
     }
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
